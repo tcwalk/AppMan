eb4a92bcd28746fd5706d670d3ff811e1e942fa8
Tom Walk <tcwalk@gmail.com>
2015-06-01 02:19:55 -0500

    Move click listener from java to xml

--- app/src/main/java/com/tropotech/appman/AppMan.java
+++ app/src/main/java/com/tropotech/appman/AppMan.java
@@ -14,42 +14,19 @@ import android.widget.Toast;
 import android.os.Bundle;
 
 
-public class AppMan  extends Activity implements View.OnClickListener {
+//public class AppMan  extends Activity implements View.OnClickListener {
+public class AppMan  extends Activity {
 
     private static final String TAG = "AppMan main activity";
-
-
-    Button spotifyButton;
-    Button scoresButton;
-    Button libraryButton;
-    Button buildItButton;
-    Button XYZReaderButton;
-    Button capstoneButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayButtons();
     }
 
-    public void displayButtons(){
-        spotifyButton = (Button) findViewById(R.id.button1);
-        spotifyButton.setOnClickListener(this);
-        scoresButton = (Button) findViewById(R.id.button2);
-        scoresButton.setOnClickListener(this);
-        libraryButton = (Button) findViewById(R.id.button3);
-        libraryButton.setOnClickListener(this);
-        buildItButton = (Button) findViewById(R.id.button4);
-        buildItButton.setOnClickListener(this);
-        XYZReaderButton = (Button) findViewById(R.id.button5);
-        XYZReaderButton.setOnClickListener(this);
-        capstoneButton = (Button) findViewById(R.id.button6);
-        capstoneButton.setOnClickListener(this);
-    }
-
-    public void onClick(View v) {
+    public void makeToast(View v) {
         Toast.makeText(this, R.string.appClick, Toast.LENGTH_LONG).show();
     }
 
@@ -57,7 +34,5 @@ public class AppMan  extends Activity implements View.OnClickListener {
     public void onDestroy() {
         super.onDestroy();
     }
-
-
 
 }
